86b2ac584112c0241221a36653ee252cdfacd010
Arbaz Karimi <arbazkarimi@gmail.com>
2024-12-10 16:36:19 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@
 
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenu = document.getElementById('mobile-menu');
+const close_menu = document.getElementById('close_menu');
 
 menuBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('!left-0');
+});
+
+close_menu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('!left-0');
 });
