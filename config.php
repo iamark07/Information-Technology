31d682d08b262b95fd8921db822777598aafd9a3
Arbaz Karimi <arbazkarimi@gmail.com>
2024-12-16 16:32:16 +0530

    add update files

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+$hostname = "http://localhost/aiims-it-website";
+$conn = mysqli_connect("localhost", "root", "", "aiims-it-website") or die("connection failed : " . mysqli_connect_error());
+?>
